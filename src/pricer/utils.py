aa68a821a4b6da58244d8f73c50ebd7602872521
bluemania <68359772+bluemania@users.noreply.github.com>
2020-09-10 18:28:06 -0700

    Bugfix/new wtf (#64)
    * working program
    
    * add item codes file
    
    * fix linting
    
    * whitespace
    
    * whitespace

--- src/pricer/utils.py
+++ src/pricer/utils.py
@@ -142,9 +142,7 @@ def get_general_settings() -> Dict[str, Any]:
 def get_and_format_auction_data(account: str = "396255466#1") -> pd.DataFrame:
     """Read raw scandata dict dump and converts to usable dataframe."""
     warcraft_path = config.us.get("warcraft_path").rstrip("/")
-    path_live = (
-        f"{warcraft_path}/WTF/Account/{account}/" + "SavedVariables/Auc-ScanData.lua"
-    )
+    path_live = f"{warcraft_path}/WTF/Account/{account}/SavedVariables/Auc-ScanData.lua"
     logger.debug(f"Loading Addon auction data from {path_live}")
 
     ropes = []
@@ -201,15 +199,10 @@ def get_and_format_auction_data(account: str = "396255466#1") -> pd.DataFrame:
     return df
 
 
-def get_item_codes() -> Dict[str, Any]:
+def get_item_codes() -> Dict[str, int]:
     """Read BeanCounter data to create code: item mapping."""
-    data = read_lua("BeanCounter")
-    item_code = {}
-    for keypart, itempart in data["BeanCounterDBNames"].items():
-        key = keypart.split(":")[0]
-        item = itempart.split(";")[1]
-        item_code[item] = key
-    return item_code
+    item_codes = pd.read_csv("data/static/items.csv", index_col="name")
+    return item_codes["entry"].to_dict()
 
 
 def write_lua(
