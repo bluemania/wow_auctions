410d347dce9640c5ee47497fa8b7cf3ff80e7de7
bluemania <68359772+bluemania@users.noreply.github.com>
2020-09-23 22:23:42 -0700

    Project redesign (#65)
    * fixing snatch stuff
    
    * fix process for running ntoebooks
    
    * change file load system
    
    * added new data injestion
    
    * working update
    
    * clean out sources and analysis
    
    * restructure write lua sections
    
    * change min price to analyse_material_cost
    
    * add deposits data
    
    * enforce columns on encoding policies
    
    * carve out the item_table portion used in policies
    
    * error with agent
    
    * move towards new single source item table
    
    * refactor config and user items
    
    * added undercut leads
    
    * add new buy policy!
    
    * tested buy policy
    
    * re-add aucscan data for latest listings
    
    * fix bug in buy policy
    
    * new style buy policy flexible using std
    
    * rename column testing
    
    * reconnect sell process for testing
    
    * adding additional character items
    
    * Added character consumes for purchasing
    
    * working item buying strategy
    
    * item updates
    
    * updated debug statements
    
    * pre sell policy changes
    
    * new sell policy
    
    * fix timing duration
    
    * add reporting module for making potions add volume change probabiltiy and fix bug add duration util
    
    * complete new sell policy
    
    * add local secrets
    
    * fix for new item adds
    
    * new material costing based on purchasing
    
    * item std changes, added sell more when -neg replenish
    
    * add clipping to pred price for wack prices
    
    * working code in good place
    
    * reformat for merge

--- src/pricer/run.py
+++ src/pricer/run.py
@@ -1,19 +1,10 @@
-"""Main entry point for pricer program.
-
-Accepts command line arguments to alter program function. Users can:
- * Initialize the program to perform setup tasks
- * Perform test runs (no data saving)
- * Perform AH analysis
- * Apply selling policies
-Declares the session datetime variable (run_dt)
-Pre-release; functionality likely to change significantly.
-"""
+"""Main entry point for pricer program."""
 import argparse
 from datetime import datetime as dt
 import logging
 import warnings
 
-from . import analysis, config, sources, utils
+from . import analysis, campaign, config as cfg, reporting, sources
 
 warnings.simplefilter(action="ignore")
 logger = logging.getLogger(__name__)
@@ -22,87 +13,55 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     """Main program runner."""
     run_dt = dt.now().replace(microsecond=0)
+    print(type(run_dt))
 
     parser = argparse.ArgumentParser(description="WoW Auctions")
-    parser.add_argument(
-        "-dp",
-        # "--deploy-pricer",
-        help="Deploy our Pricer WoW Addon with latest user specified items.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-rp",
-        # "--retrieve-pricer",
-        help="Retrieve data from our WoW Addon.",
-        action="store_true",
-    )
-    parser.add_argument("-a", help="Run primary analysis", action="store_true")
-    parser.add_argument("-t", help="Test mode (no saving)", action="store_true")
-    parser.add_argument("-s1", help="Short policy 5stack", action="store_true")
-    parser.add_argument("-s2", help="Short policy 1stack", action="store_true")
-    parser.add_argument("-m1", help="Mid policy 5stack", action="store_true")
-    parser.add_argument("-m2", help="Mid policy 1stack", action="store_true")
-    parser.add_argument("-l1", help="Long policy 5stack", action="store_true")
-    parser.add_argument(
-        "-cs", help="Flag program run as a clean session", action="store_true"
-    )
-    parser.add_argument(
-        "-played",
-        default="00d-00h-00m-00s",
-        help="""Manually specify time played in
-                        '00d-00h-00m-00s' format to calculate gold/hour""",
-    )
-    parser.add_argument(
-        "-level_time",
-        default="00d-00h-00m-00s",
-        help="""Manually specify time spent leveling in
-                        '00d-00h-00m-00s' format to subtract from
-                        gold/hour calculations""",
-    )
+    parser.add_argument("-b", help="Update web booty bay analysis", action="store_true")
+
+    parser.add_argument("-s", type=int, default=5, help="Stack size")
+    parser.add_argument("-m", type=int, default=20, help="Max sell")
+    parser.add_argument("-d", type=str, default="m", help="Duration")
+
     parser.add_argument("-v", help="Verbose mode (info)", action="store_true")
     parser.add_argument("-vv", help="Verbose mode (debug)", action="store_true")
     args = parser.parse_args()
 
-    config.set_loggers(base_logger=logger, v=args.v, vv=args.vv, test=args.t)
+    cfg.set_loggers(base_logger=logger, v=args.v, vv=args.vv)
     logger.info("Program started, arguments parsed")
-    if args.t:
-        logger.warning("TEST MODE enabled. No data saving!")
     logger.debug(args)
 
-    if args.dp:
-        utils.deploy_pricer_addon()
+    if args.b:
+        sources.get_bb_data()
 
-    if args.rp:
-        sources.retrieve_pricer_data(test=args.t)
+    sources.clean_bb_data()
+    sources.get_arkinventory_data()
+    sources.clean_arkinventory_data(run_dt)
+    sources.get_beancounter_data()
+    sources.clean_beancounter_data()
+    sources.get_auctioneer_data()
+    sources.clean_auctioneer_data()
+    sources.create_item_skeleton()
 
-    if args.a:
-        sources.create_playtime_record(
-            test=args.t,
-            run_dt=run_dt,
-            clean_session=args.cs,
-            played=args.played,
-            level_time=args.level_time,
-        )
-        sources.generate_auction_scandata(test=args.t)
-        sources.generate_auction_activity(test=args.t)
-        sources.generate_inventory(test=args.t, run_dt=run_dt)
-        analysis.analyse_item_prices()
-        analysis.analyse_sales_performance()
-        analysis.analyse_item_min_sell_price(MAT_DEV=0)
-        analysis.analyse_sell_data()
-        analysis.apply_buy_policy(MAT_DEV=0)
+    analysis.predict_item_prices()
+    analysis.analyse_listing_minprice()
+    analysis.analyse_material_cost()
+    analysis.create_item_inventory()
+    analysis.analyse_listings()
+    analysis.analyse_undercut_leads()
+    analysis.analyse_replenishment()
 
-    # Sell policies
-    if args.s1:
-        analysis.apply_sell_policy(stack=5, leads=5, duration="s")
-    if args.s2:
-        analysis.apply_sell_policy(stack=1, leads=10, duration="s")
-    if args.m1:
-        analysis.apply_sell_policy(stack=5, leads=20, duration="m")
-    if args.m2:
-        analysis.apply_sell_policy(stack=1, leads=25, duration="m")
-    if args.l1:
-        analysis.apply_sell_policy(stack=5, leads=50, duration="l")
+    analysis.create_item_table()
+
+    analysis.predict_volume_sell_probability(args.d)
+
+    campaign.analyse_buy_policy()
+    campaign.write_buy_policy()
+
+    campaign.analyse_sell_policy(stack=args.s, max_sell=args.m, duration=args.d)
+    campaign.write_sell_policy()
+
+    reporting.what_make()
+    reporting.have_in_bag()
 
     logger.info(f"Program end, seconds {(dt.now() - run_dt).total_seconds()}")
 
