6075b16d5592197d1361bc66b66b7fdd9263d617
bluemania <damnthatswack@hotmail.com>
2020-09-26 23:30:55 -0700

    set up groups properly for tsm

--- src/pricer/reporting.py
+++ src/pricer/reporting.py
@@ -16,7 +16,7 @@ def what_make() -> None:
     item_table = pd.read_parquet(path)
     item_table.index.name = 'item'
 
-    cols = ['item_id', 'made_from', 'make_pass', 'inv_total_all', 'mean_holding', 'inv_ahm_bag', 'inv_ahm_bank']
+    cols = ['item_id', 'made_from', 'make_pass', 'inv_total_all', 'mean_holding', 'inv_ahm_bag', 'inv_ahm_bank', 'Sell']
     make = item_table[cols]
 
     user_items = cfg.ui.copy()
@@ -34,13 +34,13 @@ def what_make() -> None:
     change = [True]
     while any(change):
         change = []
-        
+
         for item, row in make.iterrows():
-            
+
             made_from = user_items[item].get('made_from', {})
             under_counter = row['make_actual'] < row['make_counter']
             make_pass = row['make_pass']
-            
+
             if made_from and under_counter and not(make_pass):
                 item_increment = True
                 for material, qty in made_from.items():
@@ -53,7 +53,7 @@ def what_make() -> None:
                     make.loc[item, 'make_actual'] += 1
 
                 change.append(item_increment)
-                
+
     make_me = make[(make['make_pass']==0)&(make['make_actual']>0)]['make_actual']
     make_me.name = 'Automake'
     print(make_me)
@@ -72,8 +72,8 @@ def what_make() -> None:
     print(make_should)
 
     materials_list = make[make['make_mat_flag']==1]['item_id'].to_dict()
+    sell_items = make[make['Sell']==1]['item_id']
 
-            
 
 
     path = "/Applications/World of Warcraft/_classic_/WTF/Account/396255466#1/SavedVariables/TradeSkillMaster.lua"
@@ -89,10 +89,10 @@ def what_make() -> None:
 
     for main_key, item_data in crafting_dict['f@Alliance - Grobbulus@internalData@crafts'].items():
         queued = make_me.get(item_data.get('name'), 0)    
-        
+
         if 'queued' in item_data:
             item_data['queued'] = queued
-            
+
     new_crafting = utils.dict_to_lua(crafting_dict).encode('ascii')
     new_crafting = new_crafting.replace(b'\nf@Alliance - Grobbulus@internalData@crafts', 
                                         b'\n["f@Alliance - Grobbulus@internalData@crafts"]')
@@ -105,6 +105,11 @@ def what_make() -> None:
     item_text = '["p@Default@userData@items"] = {'
     for item_name, item_code in materials_list.items():
         item_text += f'["i:{item_code}"] = "Herbs", '
+
+    for item_name, item_code in sell_items.items():
+        if item_name not in materials_list:
+            item_text += f'["i:{item_code}"] = "Sell", '
+        
     item_text += '}'
 
     content = content[:start] + item_text.encode('ascii') + content[end:]
