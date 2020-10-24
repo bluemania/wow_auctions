45aacfb31e36a6c35a7f3c019fe99cb432036615
bluemania <68359772+bluemania@users.noreply.github.com>
2020-10-23 19:25:32 -0700

    Added tqdm progress bars (#133)
    * Added tqdm progress bars
    
    * passing tests

--- src/pricer/run.py
+++ src/pricer/run.py
@@ -25,86 +25,95 @@ def run_analytics(
     stack: int = 5, max_sell: int = 20, duration: str = "m", test: bool = False
 ) -> None:
     """Run the main analytics pipeline."""
-    with tqdm(total=220) as pbar:
+    with tqdm(total=1000, desc="Analytics") as pbar:
         run_dt = dt.now().replace(microsecond=0)
         # TODO remove this run_dt crap
         if not test:
             sources.get_arkinventory_data()
             sources.get_beancounter_data()
             sources.get_auctioneer_data()
-        pbar.update(10)
+        pbar.update(130)
 
         sources.clean_bb_data()
-        pbar.update(10)
+        pbar.update(106)
 
         sources.clean_arkinventory_data(run_dt)
-        pbar.update(10)
+        pbar.update(2)
 
         sources.clean_beancounter_data()
-        pbar.update(10)
+        pbar.update(26)
 
         sources.clean_auctioneer_data()
-        pbar.update(10)
+        pbar.update(74)
 
         sources.clean_item_skeleton()
-        pbar.update(10)
+        pbar.update(4)
 
         analysis.create_item_inventory()
-        pbar.update(10)
+        pbar.update(22)
 
         analysis.create_item_facts()
-        pbar.update(10)
+        pbar.update(115)
 
         analysis.predict_item_prices()
-        pbar.update(10)
+        pbar.update(23)
 
         analysis.analyse_rolling_buyout()
-        pbar.update(10)
+        pbar.update(146)
 
         analysis.analyse_material_cost()
-        pbar.update(10)
+        pbar.update(2)
 
         analysis.analyse_listings()
-        pbar.update(10)
+        pbar.update(204)
 
         analysis.analyse_replenishment()
-        pbar.update(10)
+        pbar.update(3)
 
         analysis.merge_item_table()
-        pbar.update(10)
+        pbar.update(4)
 
         analysis.predict_volume_sell_probability(duration)
-        pbar.update(10)
+        pbar.update(95)
 
         campaign.analyse_buy_policy()
-        pbar.update(10)
+        pbar.update(7)
 
         campaign.write_buy_policy()
-        pbar.update(10)
+        pbar.update(4)
 
         campaign.analyse_sell_policy(stack=stack, max_sell=max_sell, duration=duration)
-        pbar.update(10)
+        pbar.update(14)
 
         campaign.write_sell_policy()
-        pbar.update(10)
+        pbar.update(5)
 
         campaign.analyse_make_policy()
-        pbar.update(10)
+        pbar.update(3)
 
         campaign.write_make_policy()
-        pbar.update(10)
+        pbar.update(11)
 
 
 def run_reporting() -> None:
     """Run steps to create plots and insights."""
-    analysis.report_profits()
+    with tqdm(total=1000, desc="Reporting") as pbar:
+        analysis.report_profits()
+        pbar.update(11)
 
-    reporting.have_in_bag()
-    reporting.make_missing()
-    reporting.produce_item_reporting()
-    reporting.produce_listing_items()
-    reporting.produce_activity_tracking()
-    reporting.draw_profit_charts()
+        reporting.have_in_bag()
+        reporting.make_missing()
+        reporting.produce_item_reporting()
+        pbar.update(116)
+
+        reporting.produce_listing_items()
+        pbar.update(141)
+
+        reporting.produce_activity_tracking()
+        pbar.update(365)
+
+        reporting.draw_profit_charts()
+        pbar.update(367)
 
 
 def main() -> None:
