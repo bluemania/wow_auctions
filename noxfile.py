73da6364f1fb968f226e7de6c7f2824ed978d9f3
bluemania <damnthatswack@hotmail.com>
2020-12-09 15:39:16 -0700

    remove noxpoetry;

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,6 @@ import tempfile
 from typing import Any
 
 import nox
-import nox_poetry.patch  # noqa: F401
 from nox.sessions import Session  # noqa: I100
 
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
